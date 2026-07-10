740272d1d3947bd01ea2bdb9f4f3e34a72b58ba7
den051990 <den051990@mail.ru>
2026-07-11 00:41:05 +0700

    lesson1

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,6 @@
+first_name = "Денис"
+last_name = " Кондрацкий"
+def input():
+    print("Вас зовут: " + first_name+last_name)
+
+input()
---
+++ lesson_01/Тренировка/Тренировка.py
@@ -0,0 +1,28 @@
+my_heigh = 170
+print(my_heigh)
+
+my_name = "Денис"
+my_name = "Денис Кондрацкий"
+print(my_name)
+
+def input(pet_name):
+    print("Ваш любимчик-" + pet_name)
+
+input("Kit")
+
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
