8c182cce720c9a8d90c0138dce51afdad5971054
den051990 <den051990@mail.ru>
2026-07-20 03:38:18 +0700

    lesson3

--- lesson_01/03_lesson/lesson_3_task_2.py
+++ lesson_01/03_lesson/lesson_3_task_2.py
@@ -1,11 +1,11 @@
 from smartphone import Smartphone
 
 catalog = [
-    Smartphone("Nokia", "3310", "55555555555"),
-    Smartphone("Motorolla", "Z-5", "22222222222"),
-    Smartphone("Apple", "13", "66666666666"),
-    Smartphone("Samsyng", "Galaxy A25", "33333333333"),
-    Smartphone("Xiaomi", "Mi 11 Lite 5G", "88888888888")
+    Smartphone("Nokia", "3310", "+79235692584"),
+    Smartphone("Motorolla", "Z-5", "+79056652332"),
+    Smartphone("Apple", "13", "+79043633214"),
+    Smartphone("Samsyng", "Galaxy A25", "+79082113699"),
+    Smartphone("Xiaomi", "Mi 11 Lite 5G", "+79536622214")
 ]
 
 for smartphone in catalog:
--- lesson_01/03_lesson/user.py
+++ lesson_01/03_lesson/user.py
@@ -1,10 +1,10 @@
 class User:
-    def __init__(salf, first_name, last_name):
-        salf.first_name = first_name
-        salf.last_name = last_name
-    def get_first_name(salf):
-        return salf.first_name
-    def get_last_name(salf):
-        return salf.last_name
-    def get_first_last_name(salf):
-        return f"{salf.first_name} {salf.last_name}" 
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    def get_first_name(self):
+        return self.first_name
+    def get_last_name(self):
+        return self.last_name
+    def get_first_last_name(self):
+        return f"{self.first_name} {self.last_name}" 
